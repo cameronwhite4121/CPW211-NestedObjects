d79b21a64e616e17e6d65da39c0007db0419e225
Cameron White <cameronwhite4121@gmail.com>
2024-07-13 23:35:06 -0700

    Coded in student and advisor classes with properties

---
+++ NestedObjects/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+	/// <summary>
+	/// Represents an individual college studentS
+	/// </summary>
+	public class Student
+	{
+		/// <summary>
+		/// Legal first name
+		/// </summary>
+		public string FirstName { get; set; }
+		/// <summary>
+		/// Legal last name
+		/// </summary>
+		public string LastName { get; set; }
+		/// <summary>
+		/// Gets first name and last name concatonated together
+		/// </summary>
+		public string FullName
+		{
+			get { return FirstName + " " + LastName; }
+		}
+		/// <summary>
+		/// The students school email
+		/// </summary>
+		public string SchoolEmail { get; set; }
+		/// <summary>
+		/// Student's personal phone number
+		/// </summary>
+		public string PhoneNumber { get; set; }
+	}
+}
